348a4bbbb4743cdebb049aa6bd1d868d05a2eb3f
Alan B <918773+alanb2718@users.noreply.github.com>
2025-03-02 12:30:39 -0800

    partly working version (copied from old repo)

---
+++ src/lib/utils.ts
@@ -0,0 +1,38 @@
+import { writable } from 'svelte/store';
+// import { translations } from '../stores/localization';
+
+export const isDirty = writable(false);
+
+export function formIsDirty(initialValues: any, currentValues: any): boolean {
+  for (const key of Object.keys(initialValues)) {
+    if (!Object.prototype.hasOwnProperty.call(currentValues, key)) {
+      // If the currentData just doesn't have the field, ignore. This happens, for example,
+      // in the UserForm's password field.
+      continue;
+    }
+    if (Array.isArray(initialValues[key])) {
+      const arr1 = [...initialValues[key]].sort();
+      const arr2 = [...currentValues[key]].sort();
+      if (arr1.length !== arr2.length) {
+        isDirty.set(true);
+        return true;
+      }
+      for (const i in arr1) {
+        if (arr1[i] !== arr2[i]) {
+          isDirty.set(true);
+          return true;
+        }
+      }
+    } else if (typeof initialValues[key] === 'object') {
+      if (formIsDirty(initialValues[key], currentValues[key])) {
+        isDirty.set(true);
+        return true;
+      }
+    } else if (initialValues[key] !== currentValues[key]) {
+      isDirty.set(true);
+      return true;
+    }
+  }
+  isDirty.set(false);
+  return false;
+}
